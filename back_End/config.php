44c8fa5fb54509b542f3c3039d3448c3b824c255
bora <boracara13@gmail.com>
2025-01-10 19:50:57 +0100

    version 1

--- back_End/config.php
+++ back_End/config.php
@@ -1,14 +1,14 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = ""; // Bosh nëse nuk ke vendosur fjalëkalim në MySQL
-$dbname = "platforma_db"; // Kontrollo që ky është emri i saktë i bazës së të dhënave
+$password = ""; // Lëre bosh nëse nuk ke fjalëkalim për MySQL
+$dbname = "platforma_db"; // Emri i saktë i bazës së të dhënave
 
-$conn = new mysqli($servername, $username, $password, $dbname ); // 3306 është porti standard
-
+// Krijo lidhjen
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Kontrollo lidhjen
 if ($conn->connect_error) {
-    die("Lidhja me bazën e të dhënave dështoi: " . $conn->connect_error);
+    die("Lidhja me databazën dështoi: " . $conn->connect_error);
 }
 ?>
